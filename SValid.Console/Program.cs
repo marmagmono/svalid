8eeea92e54ff1e24662f371542cd726589e59199
marmagmono <marmagmono@gmail.com>
2019-02-26 21:44:24 +0100

    Add project files.

---
+++ SValid.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SValid.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
